7c2c4a696feda7d4087b77254c5c1b92142943dd
bogdaniordan <bogdan.iordan47@yahoo.com>
2020-12-16 13:15:21 +0200

    Add 2 weeks function

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -58,8 +58,35 @@ def get_average_age():
 
 
 def next_birthdays():
-    view.print_error_message("Not implemented yet.")
+    birth_dates = []
+    names = []
+    indexes = []
+    employees_with_birthdate_2_weeks = []
+    date_input = input('Please enter a date (e.g. 2018-02-06): ')
+    element_list = data_manager.read_table_from_file(hr.DATAFILE)
+    start_date = datetime.datetime.strptime(date_input, '%Y-%m-%d')
+    add_two_weeks = datetime.timedelta(days=14)
 
+    end_date = start_date + add_two_weeks
+    start_date = str(start_date)
+    end_date = str(end_date)
+    print(type(start_date))
+    print(type(end_date))
+    start_date = start_date[:-9]
+    end_date = end_date[:-9]
+    print(start_date)
+    print(end_date)
+    for row in element_list:
+        birth_dates.append(row[2])
+        names.append(row[1])
+    for date in birth_dates:
+        if start_date < date and date < end_date:
+            indexes.append(birth_dates.index(date))
+    for index in indexes:
+        employees_with_birthdate_2_weeks.append(names[index])
+    employeez = ''.join(employees_with_birthdate_2_weeks)
+    print(f'The emoployees with birthdays between the {start_date} and the date after 2 weeks are: {employeez}')
+        
 
 def count_employees_with_clearance():
     employees = []
@@ -73,6 +100,7 @@ def count_employees_with_clearance():
 
 
 def count_employees_per_department():
+
     element_list = data_manager.read_table_from_file(hr.DATAFILE)
     
 
