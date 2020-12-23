69348192e4c1f166bd2c9561843faf356b1d82be
bogdaniordan <bogdan.iordan47@yahoo.com>
2020-12-23 15:29:13 +0200

    Added generate random id function

--- model/util.py
+++ model/util.py
@@ -7,4 +7,17 @@ def generate_id(number_of_small_letters=4,
                 number_of_digits=2,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+    generated_id = []
+    for i in range(number_of_small_letters):
+        generated_id.append(list(string.ascii_lowercase)[random.randint(1,25)])
+    for i in range(number_of_capital_letters):
+        generated_id.append(list(string.ascii_uppercase)[random.randint(1,25)])
+    for i in range(number_of_digits):
+        generated_id.append(random.randint(0,9))
+    for i in range(number_of_special_chars):
+        generated_id.append(allowed_special_chars[random.randint(0,len(allowed_special_chars)-1)])
+    generated_id = [str(item) for item in generated_id]
+    random.shuffle(generated_id)
+    result = ''.join(generated_id)
+    return result
+
